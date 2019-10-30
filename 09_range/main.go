bb50e05725cc863e9272c7ad84e9fa5f0dc0dcc6
Ronald <ronald.scruz16@gmail.com>
2019-10-30 15:55:32 -0300

    09 done

--- 09_range/main.go
+++ 09_range/main.go
@@ -9,7 +9,25 @@ type Person struct {
 }
 
 func main() {
-	peopleInRoom := []Person{{"Ronald", 18}, {"Deschamps", 23}, {"Xulio", 22}, {"Petinho", 25}, {"Pruno", 24}}
+	peopleInRoom := []Person{{"Ronald", 18}, {"Xiofane", 27}, {"Xulio", 22}, {"Petinho", 25}, {"Pruno", 24}}
 
-	fmt.Println(peopleInRoom)
+	for personIndex, person := range peopleInRoom {
+		if person.name == "Petinho" {
+			fmt.Printf("PETINHO NO LUGAR %d\n", personIndex)
+		} else {
+			fmt.Printf("%s :l\n", person.name)
+		}
+	}
+
+	// if you don't want to get the index, just place an underscore on its name
+	for _, person := range peopleInRoom {
+		fmt.Printf("> %s\n", person.name)
+	}
+
+	// you can also iterate through maps by passing "key, value" as left-side parameters of range
+	contactList := map[string]string{"Ronald": "11 94838-9293", "Bruna": "21 98830-3019", "Toninho": "12 99289-0990"}
+
+	for name, phoneNo := range contactList {
+		fmt.Printf("Name: %s\nNumber: %s\n\n", name, phoneNo)
+	}
 }
